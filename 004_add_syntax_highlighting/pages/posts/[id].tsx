01fa4bd0c04af6ae5fffec25e3de71b89620957b
Priyash Patil <38959321+priyashpatil@users.noreply.github.com>
2022-02-15 13:30:02 +0530

    add syntax highlight example

---
+++ 004_add_syntax_highlighting/pages/posts/[id].tsx
@@ -0,0 +1,58 @@
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
+import { GetStaticProps, GetStaticPaths } from "next";
+import Link from "next/link";
+
+export default function Post({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+    categories: string[];
+  };
+}) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+          {" - "}
+          <span className="textLight">
+            {postData.categories.map((e) => (
+              <Link href={`/categories/${e.toLocaleLowerCase()}`} key={e}>
+                <a>{e}</a>
+              </Link>
+            ))}
+          </span>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
