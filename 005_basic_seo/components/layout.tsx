0f508e2d852e79aa334694dfde85d69cb696c3b4
Priyash Patil <38959321+priyashpatil@users.noreply.github.com>
2022-02-19 18:33:27 +0530

    init post 5

---
+++ 005_basic_seo/components/layout.tsx
@@ -0,0 +1,86 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+
+const name = "[Your Name]";
+export const siteTitle = "Next.js Sample Website";
+
+export default function Layout({
+  children,
+  home,
+}: {
+  children: React.ReactNode;
+  home?: boolean;
+}) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/monokai-sublime.min.css"
+          integrity="sha512-ade8vHOXH67Cm9z/U2vBpckPD1Enhdxl3N05ChXyFx5xikfqggrK4RrEele+VWY/iaZyfk7Bhk6CyZvlh7+5JQ=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <Image
+              priority
+              src="/images/profile.jpg"
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt={name}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <a>
+                <Image
+                  priority
+                  src="/images/profile.jpg"
+                  className={utilStyles.borderCircle}
+                  height={108}
+                  width={108}
+                  alt={name}
+                />
+              </a>
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/">
+                <a className={utilStyles.colorInherit}>{name}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ 005_basic_seo/lib/posts.ts
@@ -0,0 +1,150 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import remarkToc from "remark-toc";
+import rehypeSlug from "rehype-slug";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import rehypeFormat from "rehype-format";
+import rehypeSanitize, { defaultSchema } from "rehype-sanitize";
+import rehypeHighlight from "rehype-highlight";
+
+const postsDirectory = path.join(process.cwd(), "posts");
+
+export function getSortedPostsData() {
+  // Get file names under /posts
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map((fileName) => {
+    // Remove ".md" from file name to get id
+    const id = fileName.replace(/\.md$/, "");
+
+    // Read markdown file as string
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+
+    // Combine the data with the id
+    return {
+      id,
+      ...(matterResult.data as { date: string; title: string }),
+      categories: matterResult.data.categories ?? ["Uncategorized"],
+    };
+  });
+  // Sort posts by date
+  return allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+}
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ""),
+      },
+    };
+  });
+}
+
+export async function getPostData(id: string) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+
+  // Use remark to convert markdown into HTML string
+  const processedContent = await unified()
+    .data("settings", { fragment: true })
+    .use(remarkParse)
+    .use(remarkToc, { tight: true, ordered: true })
+    .use(remarkRehype)
+    .use(rehypeHighlight, { subset: false })
+    .use(rehypeSanitize, {
+      ...defaultSchema,
+      attributes: {
+        ...defaultSchema.attributes,
+        code: [
+          ...(defaultSchema.attributes?.code || []),
+          // List of all allowed languages:
+          [
+            "className",
+            "hljs",
+            "language-js",
+            "language-jsx",
+            "language-css",
+            "language-md",
+            "language-shell",
+          ],
+        ],
+        span: [
+          ...(defaultSchema.attributes!.span || []),
+          [
+            "className",
+            "hljs-addition",
+            "hljs-attr",
+            "hljs-attribute",
+            "hljs-built_in",
+            "hljs-bullet",
+            "hljs-char",
+            "hljs-code",
+            "hljs-comment",
+            "hljs-deletion",
+            "hljs-doctag",
+            "hljs-emphasis",
+            "hljs-formula",
+            "hljs-keyword",
+            "hljs-link",
+            "hljs-literal",
+            "hljs-meta",
+            "hljs-name",
+            "hljs-number",
+            "hljs-operator",
+            "hljs-params",
+            "hljs-property",
+            "hljs-punctuation",
+            "hljs-quote",
+            "hljs-regexp",
+            "hljs-section",
+            "hljs-selector-attr",
+            "hljs-selector-class",
+            "hljs-selector-id",
+            "hljs-selector-pseudo",
+            "hljs-selector-tag",
+            "hljs-string",
+            "hljs-strong",
+            "hljs-subst",
+            "hljs-symbol",
+            "hljs-tag",
+            "hljs-template-tag",
+            "hljs-template-variable",
+            "hljs-title",
+            "hljs-type",
+            "hljs-variable",
+          ],
+        ],
+      },
+    })
+    .use(rehypeStringify)
+    .use(rehypeSlug)
+    .use(rehypeFormat)
+    .process(matterResult.content);
+  const contentHtml = processedContent.toString();
+
+  // Combine the data with the id and contentHtml
+  return {
+    id,
+    contentHtml,
+    ...(matterResult.data as { date: string; title: string }),
+    categories: matterResult.data.categories ?? ["Uncategorized"],
+  };
+}
